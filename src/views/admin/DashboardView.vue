c14db3e9033abd1012b0d42139fca84b46514933
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-23 22:39:14 +0700

    feat: add dashboard view

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import MainLayout from "@/views/layouts/MainLayout.vue";
-import { useMenuStore } from "@/stores/menu";
-
-const menus = useMenuStore();
+import MainLayout from "@/views/layouts/admin/MainLayout.vue";
 </script>
 
 <template>
   <main-layout :breadcrumb="['Admin', 'Dashboard']">
     <h1>Dashboard View</h1>
-    <ul>
-      <li v-for="menu in menus?.exampleMenu" :key="menu?.key">
-        {{ menu.name }}
-      </li>
-    </ul>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam hic labore
+      similique iure a ullam laudantium eligendi. Perspiciatis optio architecto
+      aut quam cupiditate unde aspernatur!
+    </p>
   </main-layout>
 </template>
