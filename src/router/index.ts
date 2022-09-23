908fb52564beec449248ea1746b408259b58e7de
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-23 22:36:52 +0700

    feat: add route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,21 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "landing-page",
+      component: () => import("@/views/public/MainView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/public/LoginView.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/public/RegisterView.vue"),
+    },
+    {
       path: "/admin/dashboard",
       name: "dashboard",
       component: () => import("@/views/admin/DashboardView.vue"),
