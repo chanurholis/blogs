d186f6a879336e9cb791545ba79258692674c689
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-23 22:35:43 +0700

    feat: add http client

---
+++ src/http-common.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://newsapi.org/v2/",
+  headers: {
+    "Content-type": "application/json",
+    "x-api-key": "301cbe699ebe423b8b50154825aba47f",
+  },
+});
