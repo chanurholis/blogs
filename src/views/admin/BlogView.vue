27b073ca43a31084d6048497a1c29d0ad6330eb0
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-23 22:39:23 +0700

    feat: add blog view

--- src/views/admin/BlogView.vue
+++ src/views/admin/BlogView.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
-import MainLayout from "@/views/layouts/MainLayout.vue";
+import MainLayout from "@/views/layouts/admin/MainLayout.vue";
 </script>
 
 <template>
-  <main-layout :breadcrumb="['Admin', 'Blog']"><h1>Blog View</h1></main-layout>
+  <main-layout :breadcrumb="['Admin', 'Blog']">
+    <h1>Blog</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat minima
+      quam blanditiis voluptatem excepturi voluptatum sunt, dolores porro quis?
+      Velit minima quasi incidunt nam saepe minus tenetur vel quaerat in.
+    </p>
+  </main-layout>
 </template>
